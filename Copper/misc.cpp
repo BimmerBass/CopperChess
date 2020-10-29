496c6744b7df0904b2ab4189e9c454955546aa29
Niels Abildskov <niel5946@gefion-gym.dk>
2020-10-29 13:21:20 +0100

    Added cross-platform compatibility. Haven't tested it yet...

--- Copper/misc.cpp
+++ Copper/misc.cpp
@@ -1,14 +1,89 @@
+
 #include "stdio.h"
+#include "defs.h"
+
+#ifdef WIN32
+#include "windows.h"
+#else
+#include "sys/time.h"
+#include "sys/select.h"
+#include "unistd.h"
+#include "string.h"
+#endif
+
+
+
+// http://home.arcor.de/dreamlike/chess/
+int InputWaiting()
+{
+#ifndef WIN32
+  fd_set readfds;
+  struct timeval tv;
+  FD_ZERO (&readfds);
+  FD_SET (fileno(stdin), &readfds);
+  tv.tv_sec=0; tv.tv_usec=0;
+  select(16, &readfds, 0, 0, &tv);
+
+  return (FD_ISSET(fileno(stdin), &readfds));
+#else
+   static int init = 0, pipe;
+   static HANDLE inh;
+   DWORD dw;
+
+   if (!init) {
+     init = 1;
+     inh = GetStdHandle(STD_INPUT_HANDLE);
+     pipe = !GetConsoleMode(inh, &dw);
+     if (!pipe) {
+        SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT|ENABLE_WINDOW_INPUT));
+        FlushConsoleInputBuffer(inh);
+      }
+    }
+    if (pipe) {
+      if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
+      return dw;
+    } else {
+      GetNumberOfConsoleInputEvents(inh, &dw);
+      return dw <= 1 ? 0 : dw;
+	}
+#endif
+}
+
+void ReadInput(S_SEARCHINFO *info) {
+  int             bytes;
+  char            input[256] = "", *endc;
+
+    if (InputWaiting()) {
+		info->stopped = TRUE;
+		do {
+		  bytes=read(fileno(stdin),input,256);
+		} while (bytes<0);
+		endc = strchr(input,'\n');
+		if (endc) *endc=0;
+
+		if (strlen(input) > 0) {
+			if (!strncmp(input, "quit", 4))    {
+			  info->quit = TRUE;
+			}
+		}
+		return;
+    }
+}
+
+
+
+
+/*#include "stdio.h"
+
 #include "defs.h"
 
 
 #include "windows.h"
 #include "io.h"
 
-/*
-This is the reason Copper is not yet cross-platform compatible. We need to listen for input from the GUI while we're searching,
-and the code for OSX hasn't been implemented.
-*/
+//This is the reason Copper is not yet cross-platform compatible. We need to listen for input from the GUI while we're searching,
+//and the code for OSX hasn't been implemented.
+//
 
 // http://home.arcor.de/dreamlike/chess/
 int InputWaiting()
@@ -56,4 +131,4 @@ void ReadInput(S_SEARCHINFO* info) {
         }
         return;
     }
-}
+}*/
