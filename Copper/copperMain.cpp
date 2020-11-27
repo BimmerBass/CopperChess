9d56a974e3ae1fcf9ab52cda5876fe70eba99568
BimmerBass <abildskovniels@gmail.com>
2020-11-27 15:48:57 +0100

    Added WAC (Win-At-Chess) positions and their first best moves for testing

--- Copper/copperMain.cpp
+++ Copper/copperMain.cpp
@@ -1,13 +1,6 @@
 #include "defs.h"
 
 
-// IT SEVERELY BLUNDERS IN THIS POSITION WITH 2 SECONDS MOVETIME
-#define FUCKyouFEN "3rr3/pbp4p/2p1kpp1/4n1P1/2p1Pp1P/P2P1N2/1PPB2K1/3RR3 w - - 0 36"
-
-// IT GIVES AWAY THE QUEEN IN THIS POSITION WITH 5 SECONDS MOVETIME
-#define WEIRD "2b2rk1/p3qppp/5n2/4p1B1/7Q/2N3P1/Pr2PP1P/R3K2R w KQ - 0 16"
-
-
 int main() {
 	initAll(SetMask, ClearMask);
 
